0facbb3e26e0a01ab1f7a8157b7c9c50707b4714
d1k4s <dikaest09@gmail.com>
2023-05-15 16:15:31 +0700

    Trial Image Slider

--- app/src/main/java/com/example/daiscrivi_mobileapp_semester4/MainActivity.java
+++ app/src/main/java/com/example/daiscrivi_mobileapp_semester4/MainActivity.java
@@ -3,24 +3,28 @@ package com.example.daiscrivi_mobileapp_semester4;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.daiscrivi_mobileapp_semester4.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    Button Log_out;
+    ViewPager2 viewPager2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_main);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());

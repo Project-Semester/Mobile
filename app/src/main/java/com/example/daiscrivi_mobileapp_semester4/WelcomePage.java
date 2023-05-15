84c2946fab0d84004936d8465b63e018eb0a1e5d
Fagil Nuril Akbar <fagilnuril18@gmail.com>
2023-05-15 15:25:03 +0700

    login bek-en

--- app/src/main/java/com/example/daiscrivi_mobileapp_semester4/WelcomePage.java
+++ app/src/main/java/com/example/daiscrivi_mobileapp_semester4/WelcomePage.java
@@ -1,14 +1,42 @@
 package com.example.daiscrivi_mobileapp_semester4;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WelcomePage extends AppCompatActivity {
+    Button btn_login, btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_welcome_page);
+        btn_login = findViewById(R.id.btnLogin);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomePage.this, login.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_register = findViewById(R.id.btnRegister);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomePage.this, register.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }

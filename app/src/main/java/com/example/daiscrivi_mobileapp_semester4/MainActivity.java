48cc47ff1b0a14268bdb0403951c0bee314c1b20
d1k4s <dikaest09@gmail.com>
2023-05-16 23:01:48 +0700

    fix bug cannot open app

--- app/src/main/java/com/example/daiscrivi_mobileapp_semester4/MainActivity.java
+++ app/src/main/java/com/example/daiscrivi_mobileapp_semester4/MainActivity.java
@@ -3,44 +3,42 @@ package com.example.daiscrivi_mobileapp_semester4;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.daiscrivi_mobileapp_semester4.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
-    Button Log_out;
-    ViewPager2 viewPager2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         replaceFragment(new HomeFragment());
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-            if (item.getItemId() == R.id.home) {
-                replaceFragment(new HomeFragment());
-            } else if (item.getItemId() == R.id.search) {
-                replaceFragment(new SearchFragment());
-            } else if (item.getItemId() == R.id.write) {
-                replaceFragment(new WriteFragment());
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.search:
+                    replaceFragment(new SearchFragment());
+                    break;
+                case R.id.write:
+                    replaceFragment(new WriteFragment());
+                    break;
             }
-
             return true;
-
         });
 
     }

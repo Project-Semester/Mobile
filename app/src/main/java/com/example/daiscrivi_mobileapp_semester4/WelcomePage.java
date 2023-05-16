e98fbe86c60442cf2a09552dad76debf12b24561
Fagil Nuril Akbar <fagilnuril18@gmail.com>
2023-05-16 20:50:17 +0700

    fix gradle 2

--- app/src/main/java/com/example/daiscrivi_mobileapp_semester4/WelcomePage.java
+++ app/src/main/java/com/example/daiscrivi_mobileapp_semester4/WelcomePage.java
@@ -2,12 +2,14 @@ package com.example.daiscrivi_mobileapp_semester4;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
 
 public class WelcomePage extends AppCompatActivity {
     Button btn_login, btn_register;
@@ -15,27 +17,18 @@ public class WelcomePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_welcome_page);
         btn_login = findViewById(R.id.btnLogin);
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(WelcomePage.this, login.class);
-                startActivity(intent);
-            }
+        btn_login.setOnClickListener(v -> {
+            Intent intent = new Intent(WelcomePage.this, login.class);
+            startActivity(intent);
         });
 
         btn_register = findViewById(R.id.btnRegister);
-        btn_register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(WelcomePage.this, register.class);
-                startActivity(intent);
-            }
-        });
+        btn_register.setOnClickListener(v -> Toast.makeText(WelcomePage.this, "Email atau Password salah", Toast.LENGTH_SHORT).show());
 
 
     }
--- app/src/main/java/com/example/daiscrivi_mobileapp_semester4/login.java
+++ app/src/main/java/com/example/daiscrivi_mobileapp_semester4/login.java
@@ -4,39 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Objects;
+
 public class login extends AppCompatActivity {
-EditText et_email, et_password;
-Button btn_login;
+    EditText et_email, et_password;
+    Button btn_login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_login);
-        et_email= findViewById(R.id.etEmail);
-        et_password= findViewById(R.id.etPassword);
-        btn_login= findViewById(R.id.btnLogin);
+        et_email = findViewById(R.id.etEmail);
+        et_password = findViewById(R.id.etPassword);
+        btn_login = findViewById(R.id.btnLogin);
 
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = et_email.getText().toString();
-                String password = et_password.getText().toString();
-                if (email.equals("fagilnuril18@gmail.com") && password.equals("123")){
-                    Intent intent = new Intent(login.this, MainActivity.class);
-                    startActivity(intent);
-                }
-                else {
-                    Toast.makeText(login.this, "Email atau Password salah", Toast.LENGTH_SHORT).show();
-                }
+        btn_login.setOnClickListener(v -> {
+            String email = et_email.getText().toString();
+            String password = et_password.getText().toString();
+            if (email.equals("fagilnuril18@gmail.com") && password.equals("123")) {
+                Intent intent = new Intent(login.this, MainActivity.class);
+                startActivity(intent);
+            } else {
+                Toast.makeText(login.this, "Email atau Password salah", Toast.LENGTH_SHORT).show();
             }
         });
 

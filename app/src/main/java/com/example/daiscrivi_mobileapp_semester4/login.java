1c22e11dcaa8f74bf4b5a5d02adf7e4bbc1de19b
Fagil Nuril Akbar <fagilnuril18@gmail.com>
2023-05-16 22:24:44 +0700

    fix crash (cant)

--- app/src/main/java/com/example/daiscrivi_mobileapp_semester4/login.java
+++ app/src/main/java/com/example/daiscrivi_mobileapp_semester4/login.java
@@ -2,42 +2,22 @@ package com.example.daiscrivi_mobileapp_semester4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+
 import android.os.Bundle;
 
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import java.util.Objects;
+
 
 public class login extends AppCompatActivity {
-    EditText et_email, et_password;
-    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        Objects.requireNonNull(getSupportActionBar()).hide();
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
-        et_email = findViewById(R.id.etEmail);
-        et_password = findViewById(R.id.etPassword);
-        btn_login = findViewById(R.id.btnLogin);
-
-        btn_login.setOnClickListener(v -> {
-            String email = et_email.getText().toString();
-            String password = et_password.getText().toString();
-            if (email.equals("fagilnuril18@gmail.com") && password.equals("123")) {
-                Intent intent = new Intent(login.this, MainActivity.class);
-                startActivity(intent);
-            } else {
-                Toast.makeText(login.this, "Email atau Password salah", Toast.LENGTH_SHORT).show();
-            }
-        });
-
     }
 }
